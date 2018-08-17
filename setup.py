06297f1511c6bf27e4246bb17ce459a8e2bc035f
Caleb Fangmeier <caleb@fangmeier.tech>
2018-08-17 12:19:26 -0500

    Press ctrl-q to queue all songs in search window. Adds special "liked" playlist.

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md') as f:
 
 setup(
     name='tuijam',
-    version='0.2.8',
+    version='0.2.9',
     description='A fancy TUI client for Google Play Music',
     long_description=desc,
     long_description_content_type='text/markdown',
