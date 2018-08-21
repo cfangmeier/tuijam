4f0820550b597df6cd86848fb8abaf2c007a80d3
Caleb Fangmeier <caleb@fangmeier.tech>
2018-08-21 13:41:19 -0500

    More universal thumbs up/down symbols, simplifies colorscheme

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open('README.md') as f:
 
 setup(
     name='tuijam',
-    version='0.2.9',
+    version='0.2.10',
     description='A fancy TUI client for Google Play Music',
     long_description=desc,
     long_description_content_type='text/markdown',
