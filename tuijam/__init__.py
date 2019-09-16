06ee04ddb2ea5c2c72ae5141554c87f9e89e0660
Caleb Fangmeier <caleb@fangmeier.tech>
2019-09-16 17:54:58 -0500

    bump to 0.5.7

--- tuijam/__init__.py
+++ tuijam/__init__.py
@@ -1,6 +1,6 @@
 from os.path import join, expanduser
 
-__version__ = "0.5.6"
+__version__ = "0.5.7"
 CONFIG_DIR = join(expanduser("~"), ".config", "tuijam")
 CONFIG_FILE = join(CONFIG_DIR, "config.yaml")
 LOG_FILE = join(CONFIG_DIR, "log.txt")
