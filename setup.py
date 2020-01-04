d04ac090517645ca991fbb1ab7fe6a36fb8b362e
Yury Kurlykov <sh1ftr@protonmail.ch>
2020-01-04 20:12:18 +1000

    setup: Compile translations while building

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
+from setuptools.command.install import install
 from tuijam import __version__
 
 import sys
@@ -12,6 +13,16 @@ with open("requirements.txt") as f:
 
 with open("README.md") as f:
     desc = f.read()
+
+class PostInstallCommand(install):
+    def run(self):
+        from babel.messages.frontend import compile_catalog
+        compiler = compile_catalog(self.distribution)
+        option_dict = self.distribution.get_option_dict('compile_catalog')
+        compiler.domain = [option_dict['domain'][1]]
+        compiler.directory = option_dict['directory'][1]
+        compiler.run()
+        super().run()
 
 setup(
     name="tuijam",
@@ -36,6 +47,10 @@ setup(
     ],
     keywords="terminal music streaming",
     install_requires=requirements,
+    setup_requires=['babel',],
     packages=find_packages(),
     entry_points={"console_scripts": ["tuijam=tuijam.app:main"]},
+    cmdclass={"install": PostInstallCommand,},
+    package_data={'tuijam': ['lang/*/*/*.mo', 'lang/*/*/*.po']},
+    include_package_data=True,
 )
